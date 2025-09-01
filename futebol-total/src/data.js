8461f8781ef1fb378506122355d137e999b7fbe5
Tutuzxx <kauar1253@gmail.com>
2025-08-31 22:53:46 -0300

    Add files via upload

---
+++ futebol-total/src/data.js
@@ -0,0 +1,47 @@
+export const articles = [
+  {
+    title: "Flamengo empata com Grêmio e mantém liderança",
+    summary: "Partida movimentada termina 1x1 no Maracanã, com recorde de público.",
+    source: "ge.globo",
+    league: "Brasileirão",
+    link: "https://ge.globo.com",
+    image: "https://i.imgur.com/jpQmZpK.jpg",
+    publishedAt: "2025-08-31T18:00:00-03:00"
+  },
+  {
+    title: "Manchester City vence Brighton fora de casa",
+    summary: "Com gol de Haaland, City segue firme na briga pelo título da Premier League.",
+    source: "ESPN",
+    league: "Premier League",
+    link: "https://espn.com.br",
+    image: "https://i.imgur.com/m6nU5o2.jpg",
+    publishedAt: "2025-08-31T16:00:00-03:00"
+  },
+  {
+    title: "Corinthians e Palmeiras empatam em clássico eletrizante",
+    summary: "Derby paulista terminou 2x2 na Neo Química Arena.",
+    source: "UOL Esporte",
+    league: "Brasileirão",
+    link: "https://uol.com.br/esporte",
+    image: "https://i.imgur.com/3XlwMlJ.jpg",
+    publishedAt: "2025-08-31T19:00:00-03:00"
+  },
+  {
+    title: "Real Madrid estreia com vitória na Champions League",
+    summary: "Time espanhol bateu o Milan por 2x1 no Santiago Bernabéu na primeira rodada.",
+    source: "Marca",
+    league: "Champions League",
+    link: "https://marca.com",
+    image: "https://i.imgur.com/vXqFmc2.jpg",
+    publishedAt: "2025-08-30T17:00:00-03:00"
+  },
+  {
+    title: "Brasil convoca reforços para Eliminatórias",
+    summary: "Seleção Brasileira chama novos jogadores para os jogos contra Argentina e Chile.",
+    source: "CBF",
+    league: "Seleção",
+    link: "https://cbf.com.br",
+    image: "https://i.imgur.com/M6cLz7H.jpg",
+    publishedAt: "2025-08-29T12:00:00-03:00"
+  }
+];
